0ccace4b8a1121bc51fa457f08e4e202378337dd
norrie-adams <normanadams087@gmail.com>
2026-07-11 13:04:24 -0400

    Added player movement and cleaned Unity project

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        
+        Vector3 movement = new Vector3(horizontalInput, 0.0f, verticalInput);
+        
+        transform.Translate(movement * moveSpeed * Time.deltaTime);
+    }
+}
